20281a3db43c478160db379dd57c3b3e63d1e028
Nathan Drake <nathandrakedrums@gmail.com>
2022-01-06 18:05:25 -0600

    taking a break

--- script.js
+++ script.js
@@ -6,6 +6,28 @@ const botDoorPath = "https://content.codecademy.com/projects/chore-door/images/r
 const beachDoorPath = "https://content.codecademy.com/projects/chore-door/images/beach.svg";
 const spaceDoorPath = "https://content.codecademy.com/projects/chore-door/images/space.svg";
 
+const numClosedDoors = 3;
+
+let openDoor1;
+let openDoor2;
+let openDoor3;
+
+const randomChoreDoorGenerator = () => {
+  const choreDoor = Math.floor(Math.random() * numClosedDoors)
+
+  switch (choreDoor) {
+    case 1:
+      
+      break;
+    case 2:
+
+      break;
+    case 3:
+
+      break;
+  }
+}
+
 doorImage1.onclick = () => {
   doorImage1.src = botDoorPath;
 }
